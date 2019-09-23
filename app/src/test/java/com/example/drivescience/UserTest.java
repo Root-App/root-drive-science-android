4a5244f1cfe0c9d479b7a04e408f82a4ca75cb0b
Jordan Sanders <jordan.sanders@joinroot.com>
2019-09-23 15:11:16 -0500

    Persist a user in shared preferences
    https://developer.android.com/training/data-storage/shared-preferences

--- app/src/test/java/com/example/drivescience/UserTest.java
+++ app/src/test/java/com/example/drivescience/UserTest.java
@@ -1,17 +1,32 @@
 package com.example.drivescience;
 
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(RobolectricTestRunner.class)
 public class UserTest {
+
+    private User subject;
+
+    @Before
+    public void setUp() {
+        Context context = ApplicationProvider.getApplicationContext();
+        subject = new User(context);
+    }
 
     @Test
     public void hasRootDriverToken() {
-        User user = new User();
-        assertFalse(user.hasRootDriverToken());
+        assertFalse(subject.hasRootDriverToken());
 
-        user.setRootDriverToken("a token");
-        assertTrue(user.hasRootDriverToken());
+        subject.setRootDriverToken("a token");
+        assertTrue(subject.hasRootDriverToken());
     }
 }
