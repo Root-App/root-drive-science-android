36d88a04caff9e8eb99630905dd95630f78e5a9f
Jordan Sanders <jordan.sanders@joinroot.com>
2019-09-23 15:48:02 -0500

    Add user management buttons

--- app/src/main/java/com/example/drivescience/UserManager.java
+++ app/src/main/java/com/example/drivescience/UserManager.java
@@ -3,13 +3,13 @@ package com.example.drivescience;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-public class User {
+public class UserManager {
     String TOKEN = "token";
 
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
 
-    public User(Context context) {
+    public UserManager(Context context) {
         preferences = context.getSharedPreferences(
                 context.getString(R.string.user_preferences),
                 context.MODE_PRIVATE
@@ -27,6 +27,11 @@ public class User {
 
     public void setRootDriverToken(String token) {
         editor.putString(TOKEN, token);
+        editor.commit();
+    }
+
+    public void clearRootDriverToken() {
+        editor.remove(TOKEN);
         editor.commit();
     }
 }
